8640e0da0a7504723915afaa7e7818584733a310
opticka <opticka@optickaIII>
2013-01-02 10:38:24 +0000

    add small training control script, can now drive reward from eyelink calibration routine using space to accept a fixation position

---
+++ calibration/calibrationTraining.m
@@ -0,0 +1,33 @@
+classdef calibrationTraining < optickaCore
+	%UNTITLED Summary of this class goes here
+	%   Detailed explanation goes here
+	
+	properties
+		eM = []
+		family = 'trainer'
+		verbose = true
+	end
+	
+	methods
+		function obj = calibrationTraining(obj)
+			global lj
+			if ~isa(lj,'labJack')
+				lj = labJack('verbose',true,'name','calibTrain');
+			end
+			obj.eM = eyelinkManager('name','calibTrain');
+			obj.eM.calibrationStyle = 'HV9';
+			%runDemo(obj.eM)
+		end
+		
+		function run(obj)
+			if isa(obj.eM,'eyelinkManager')
+				obj.eM.modify.calibrationtargetcolour = [1 1 0];
+				obj.eM.modify.calibrationtargetsize = 5;
+				obj.eM.modify.calibrationtargetwidth = 3;
+				obj.eM.modify.waitformodereadytime = 500;
+				runDemo(obj.eM)
+			end
+		end
+	end
+	
+end
