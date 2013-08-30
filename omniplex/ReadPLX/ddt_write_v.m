5932be660916c22fa776ae3524003d839f811617
ian <ian@mlion.leviathan>
2013-08-30 19:48:17 +0100

    update plexon plx/pl2 SDK and improve support for PL2 files.

--- omniplex/ReadPLX/ddt_write_v.m
+++ omniplex/ReadPLX/ddt_write_v.m
@@ -4,24 +4,24 @@ function [errCode] = ddt_write_v(filename, nch, npoints, freq, d)
 % [errCode] = ddt_write_v(filename, nch, npoints, freq, d)
 %
 % INPUT:
-%   filename - if empty string, will use File Open dialog
+%    filename - if empty string, will use File Open dialog
 %    nch - number of channels
-%   npoints - number of data points per channel
+%    npoints - number of data points per channel
 %    freq - data frequency in Hz
 %    d - [nch npoints] data array (in mV)
 %
 % OUTPUT:
-%   errCode - error code: 1 for success, 0 for failure
+%    errCode - error code: 1 for success, 0 for failure
 
 if nargin < 5
     error 'Expected 5 input arguments';
 end
 if (isempty(filename))
-   [fname, pathname] = uiputfile('*.ddt', 'Specify .ddt file');
-   if isequal(fname,0)
-     error 'No file was specified'
-   end
-   filename = fullfile(pathname, fname);
+    [fname, pathname] = uiputfile('*.ddt', 'Specify .ddt file');
+    if isequal(fname,0)
+        error 'No file was specified'
+    end
+    filename = fullfile(pathname, fname);
 end
 
 [errCode] = mexPlex(25, filename, nch, npoints, freq, d)
--- omniplex/ReadPLX/readall.m
+++ omniplex/ReadPLX/readall.m
@@ -82,8 +82,8 @@ if ( nslowchannels > 0 )
     allad = cell(1,nslowchannels);
     for ich = 0:nslowchannels-1
         if ( slowcounts(ich+1) > 0 )
-            [adfreq, nad, tsad, fnad, allad{ich+1}] = plx_ad(OpenedFileName, ich);
-            numads = numads + 1;
+			[adfreq, nad, tsad, fnad, allad{ich+1}] = plx_ad(OpenedFileName, ich);
+			numads = numads + 1;
         end
     end
 end
@@ -113,15 +113,15 @@ end
 if ( nevchannels > 0 ) 
     % need the event chanmap to make any sense of these
     [u,evchans] = plx_event_chanmap(OpenedFileName);
-    for iev = 1:nevchannels
-        if ( evcounts(iev) > 0 )
+	for iev = 1:nevchannels
+		if ( evcounts(iev) > 0 )
             evch = evchans(iev);
             if ( evch == 257 )
-                [nevs{iev}, tsevs{iev}, svStrobed] = plx_event_ts(OpenedFileName, evch); 
-            else
-                [nevs{iev}, tsevs{iev}, svdummy] = plx_event_ts(OpenedFileName, evch);
+				[nevs{iev}, tsevs{iev}, svStrobed] = plx_event_ts(OpenedFileName, evch); 
+			else
+				[nevs{iev}, tsevs{iev}, svdummy] = plx_event_ts(OpenedFileName, evch);
             end
-        end
-    end
+		end
+	end
 end
 [nev,evnames] = plx_event_names(OpenedFileName);
